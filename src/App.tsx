fb87ad21efa54b1f86679adaa555a73de4efb5ed
burak4arslan <burak4arslan@gmail.com>
2022-01-13 21:48:20 +0300

    Added Creating, Editing and Deleting Todos

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
+import TodoComponent from "./components/TodoComponent";
+import _uniqueId from "lodash/uniqueId";
 
 function App() {
+
+    const [todoIdList, setTodoIdList] = useState<string[]>([]);
+
+    const addTodoClicked = () => {
+        setTodoIdList(todoIdList.concat([_uniqueId("todo-")]));
+    };
+
+    const deleteClicked = (idToDelete: string) => {
+        setTodoIdList(todoIdList.filter(id => id !== idToDelete));
+    };
+
     return (
         <div className="App">
-
+            <div className={"todo-list-box"}>
+                <h1>TODOS</h1>
+                <button className={"add-todo-button"} onClick={() => addTodoClicked()}>Add To-do</button>
+                {todoIdList.map((id) => {
+                    return <TodoComponent id={id} deleteClicked={(idToDelete: string) => deleteClicked(idToDelete)}
+                                          key={id}/>
+                })
+                }
+            </div>
         </div>
     );
 }
--- src/components/TodoComponent.tsx
+++ src/components/TodoComponent.tsx
@@ -1,16 +1,48 @@
-interface todoInterface {
-    name: string,
-    detail: string,
-    status: string
+import {useState} from "react";
+
+interface TodoComponentProps {
+    id: string,
+    deleteClicked: (id: string) => void
 }
 
-const TodoComponent = (props: todoInterface) => {
+const TodoComponent = (props: TodoComponentProps) => {
 
-    return <div>
-        <h1>{props.name}</h1>
-        <span>{props.detail}</span>
+    const [name, setName] = useState(props.id);
+    const [tempName, setTempName] = useState("burak");
+    const [detail, setDetail] = useState<string>("detail");
+    const [tempDetail, setTempDetail] = useState<string>("detail");
+    const [status, setStatus] = useState("normal");
+
+    const okClicked = () => {
+        setName(tempName);
+        setDetail(tempDetail);
+        setStatus("normal")
+    };
+
+    const editClicked = () => {
+        setTempName(name);
+        setTempDetail(detail);
+        setStatus("editing")
+    };
+
+    const deleteClicked = () => {
+        props.deleteClicked(props.id);
+    };
+
+    if (status === "editing") {
+        return <div className={"todo-box"}>
+            <h1><input value={tempName} onChange={(e) => setTempName(e.target.value)}/></h1>
+            <span><input value={tempDetail} onChange={(e) => setTempDetail(e.target.value)}/></span>
+            <button className={"ok-button"} onClick={() => okClicked()}>Ok</button>
+            <button className={"cancel-button"} onClick={() => setStatus("normal")}>Cancel</button>
+        </div>
+    }
+    return <div className={"todo-box"}>
+        <h1>{name}</h1>
+        <span>{detail}</span>
+        <button className={"edit-button"} onClick={() => editClicked()}>Edit</button>
+        <button className={"delete-button"} onClick={() => deleteClicked()}>Delete</button>
     </div>
 };
-
 
 export default TodoComponent;

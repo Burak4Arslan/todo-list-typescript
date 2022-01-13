7c5614d24120ac750d25a36f3afca8ca8eedb45e
burak4arslan <burak4arslan@gmail.com>
2022-01-13 22:22:53 +0300

    Css updates

--- src/components/TodoComponent.tsx
+++ src/components/TodoComponent.tsx
@@ -1,4 +1,4 @@
-import {useState} from "react";
+import React,{useState} from "react";
 
 interface TodoComponentProps {
     id: string,
@@ -12,6 +12,7 @@ const TodoComponent = (props: TodoComponentProps) => {
     const [detail, setDetail] = useState<string>("detail");
     const [tempDetail, setTempDetail] = useState<string>("detail");
     const [status, setStatus] = useState("normal");
+    const [backgroundColor, setBackgroundColor] = useState(`rgba(${Math.random() * (240 - 80) + 80},${Math.random() * (200 - 80) + 80},${Math.random() * (200 - 80) + 80},1)`);
 
     const okClicked = () => {
         setName(tempName);
@@ -30,14 +31,14 @@ const TodoComponent = (props: TodoComponentProps) => {
     };
 
     if (status === "editing") {
-        return <div className={"todo-box"}>
-            <h1><input value={tempName} onChange={(e) => setTempName(e.target.value)}/></h1>
-            <span><input value={tempDetail} onChange={(e) => setTempDetail(e.target.value)}/></span>
+        return <div className={"todo-box"} style={{backgroundColor: backgroundColor}}>
+            <h1><input className={"todo-name-input"} value={tempName} onChange={(e) => setTempName(e.target.value)}/></h1>
+            <span><input className={"todo-detail-input"} value={tempDetail} onChange={(e) => setTempDetail(e.target.value)}/></span>
             <button className={"ok-button"} onClick={() => okClicked()}>Ok</button>
             <button className={"cancel-button"} onClick={() => setStatus("normal")}>Cancel</button>
         </div>
     }
-    return <div className={"todo-box"}>
+    return <div className={"todo-box"} style={{backgroundColor: backgroundColor}}>
         <h1>{name}</h1>
         <span>{detail}</span>
         <button className={"edit-button"} onClick={() => editClicked()}>Edit</button>
